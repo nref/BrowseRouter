c1abc69028406bdce72ebdc3960c9b81cf511dee
Doug Slater <doug@slater.dev>
2022-07-07 16:05:11 -0400

    Refactor BrowserService

---
+++ App/UriFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace BrowseRouter;
+
+public static class UriFactory
+{
+  public static Uri Get(string url)
+  {
+    try
+    {
+      return new Uri(url);
+    }
+    catch (UriFormatException)
+    {
+      // Try to prepend https when given an incomplete URI e.g. "google.com"
+      return new Uri($"https://{url}");
+    }
+  }
+}
---
+++ App/UrlPreferenceExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BrowseRouter;
+
+public static class UrlPreferenceExtensions
+{
+  public static bool TryGetPreference(this IEnumerable<UrlPreference> prefs, Uri uri, out UrlPreference pref)
+  {
+    pref = prefs.FirstOrDefault(pref =>
+    {
+      (string domain, string pattern) = pref.GetDomainAndPattern(uri);
+      return Regex.IsMatch(domain, pattern);
+    })!;
+
+    return pref != null;
+  }
+
+  public static (string, string) GetDomainAndPattern(this UrlPreference pref, Uri uri)
+  {
+    string urlPattern = pref.UrlPattern;
+
+    if (urlPattern.StartsWith("/") && urlPattern.EndsWith("/"))
+    {
+      // The domain from the INI file is a regex
+      string domain = uri.Authority + uri.AbsolutePath;
+      string pattern = urlPattern.Substring(1, urlPattern.Length - 2);
+
+      return (domain, pattern);
+    }
+
+    {
+      // We're only checking the domain.
+      string domain = uri.Authority;
+
+      // Escape the input for regex; the only special character we support is a *
+      var regex = Regex.Escape(urlPattern);
+
+      // Unescape * as a wildcard.
+      string pattern = $"^{regex.Replace("\\*", ".*")}$";
+
+      return (domain, pattern);
+    }
+  }
+}
