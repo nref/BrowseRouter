cc528292492f24aeaf51d0b780e8809594df409f
Martin Rubli <martin@rubli.info>
2023-02-25 16:18:58 +0100

    App: Rewrite ComputeExePath to support Windows and Linux
    The new method works with normal builds and single file publishing.

--- App/App.cs
+++ App/App.cs
@@ -6,17 +6,42 @@ public static class App
 {
   static string ComputeExePath()
   {
-    var assembly = Assembly.GetExecutingAssembly();
-    // .NET Core returns BrowserSettings.dll but we need BrowserSettings.exe
-    var exePath = Assembly.GetExecutingAssembly().Location
-        .Replace(".dll", ".exe");
-    if (!string.IsNullOrEmpty(exePath))
-    {
-      return exePath;
-    }
+    // There exist a variety of methods to obtain the executable path:
+    //
+    // System.Diagnostics.Process.GetCurrentProcess().MainModule?.FileName
+    //   Windows, normal build:           …\bin\Debug\net6.0\BrowseRouter.exe
+    //   Windows, single file publishing: …\bin\Debug\net6.0\win-x64\BrowseRouter.exe
+    //   Linux, normal build:             …/bin/Debug/net6.0/BrowseRouter
+    //   Linux, single file publishing:   …/bin/Debug/net6.0/linux-x64/publish/BrowseRouter
+    //   Renames of the executable are also handled correctly by this method.
+    //
+    // Assembly.GetExecutingAssembly().Location
+    //   Windows, normal build:           …\bin\Debug\net6.0\BrowseRouter.dll
+    //   Windows, single file publishing: …\bin\Debug\net6.0\win-x64\BrowseRouter.dll
+    //   Linux, normal build:             …/bin/Debug/net6.0/BrowseRouter.dll
+    //   Linux, single file publishing:   (null)
+    //
+    // AppContext.BaseDirectory
+    // AppDomain.CurrentDomain.BaseDirectory
+    //   Windows, normal build:           …\bin\Debug\net6.0\
+    //   Windows, single file publishing: …\bin\Debug\net6.0\win-x64\
+    //   Linux, normal build:             …/bin/Debug/net6.0/
+    //   Linux, single file publishing:   …/bin/Debug/net6.0/linux-x64/publish/
+    //
+    // GetCurrentProcess().MainModule is the most reliable one and works across platforms.
+    // Theoretically it seems possible for MainModule or MainModule.FileName to be null, so we
+    // provide a fallback based on AppDomain.CurrentDomain.BaseDirectory as well.
 
+    var module = System.Diagnostics.Process.GetCurrentProcess().MainModule;
+    if (module != null && module.FileName != null)
+      return module.FileName;
+
+    // Fallback method
     var dir = AppDomain.CurrentDomain.BaseDirectory;
-    return Path.Combine(dir, FriendlyName + ".exe");
+    var ext = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows)
+            ? ".exe"
+            : "";
+    return Path.Combine(dir, FriendlyName + ext);
   }
 
   public static string FriendlyName { get; } = AppDomain.CurrentDomain.FriendlyName;
