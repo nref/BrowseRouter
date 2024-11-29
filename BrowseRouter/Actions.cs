4d26176e50b5bdc438494101419e8cbde210e448
Doug Slater <doug@slater.dev>
2024-11-29 10:10:49 -0500

    Really fix 10s delay on Win10

---
+++ BrowseRouter/Actions.cs
@@ -0,0 +1,18 @@
+﻿namespace BrowseRouter;
+
+public static class Actions
+{
+  public static bool TryRun(Action a)
+  {
+    try
+    {
+      a();
+      return true;
+    }
+    catch (Exception e)
+    {
+      Log.Write($"{e}");
+      return false;
+    }
+  }
+}
