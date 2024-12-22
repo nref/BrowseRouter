82b368f23dfe7cf1caab8ca1fef8c8d91edf8ec7
Pamphile <pamphile.saltel@wanadoo.fr>
2024-12-22 04:44:23 +0100

    restore {url} tag functionnality in browser arguments and added a few more useful tags at the same time

--- BrowseRouter/Executable.cs
+++ BrowseRouter/Executable.cs
@@ -1,4 +1,6 @@
-﻿namespace BrowseRouter;
+﻿using System.Text;
+
+namespace BrowseRouter;
 
 public static class Executable
 {
@@ -22,5 +24,84 @@ public static class Executable
 
     // The single executable without any other arguments.
     return (s, "");
+  }
+
+  /// <summary>
+  /// Complete the arguments to call the browser with, with the uri requested and according to the recognized tags in them.
+  /// If no tags is recognized the uri is simply added to the end of the arguments.
+  /// </summary>
+  /// <param name="originalArgs">The unformatted arguments</param>
+  /// <param name="uri">The URI to format the arguments with</param>
+  /// <returns>The formatted arguments</returns>
+  public static string FormatArguments(string originalArgs, Uri uri)
+  {
+    int tagReplacedCount = 0;
+    StringBuilder args = new();
+
+    int nextIndexToAdd = 0;
+    int tagStartIndex = originalArgs.IndexOf('{');
+
+    while (tagStartIndex >= 0)
+    {
+      args.Append(originalArgs.AsSpan(nextIndexToAdd, tagStartIndex - nextIndexToAdd));
+      nextIndexToAdd = tagStartIndex;
+
+      int tagEndIndex = originalArgs.IndexOf('}', tagStartIndex);
+      if (tagEndIndex < 0) // if there's no more '}' in the rest of the args string
+        break;
+
+      bool successfullyReplacedTag = true;
+
+      ReadOnlySpan<char> tagContent = originalArgs.AsSpan(tagStartIndex + 1, tagEndIndex - 1 - tagStartIndex);
+      switch (tagContent)
+      {
+        case "url":
+          args.Append(uri.OriginalString);
+          break;
+        case "userinfo":
+          args.Append(uri.UserInfo);
+          break;
+        case "host":
+          args.Append(uri.Host);
+          break;
+        case "port":
+          args.Append(uri.Port);
+          break;
+        case "authority":
+          args.Append(uri.Authority);
+          break;
+        case "path":
+          args.Append(uri.AbsolutePath);
+          break;
+        case "query":
+          args.Append(uri.Query);
+          break;
+        case "fragment":
+          args.Append(uri.Fragment);
+          break;
+
+        default:
+          successfullyReplacedTag = false;
+          break;
+      }
+
+      if (successfullyReplacedTag)
+      {
+        tagReplacedCount++;
+
+        nextIndexToAdd = tagEndIndex + 1;
+        tagStartIndex = originalArgs.IndexOf('{', tagEndIndex);
+      }
+      else
+        tagStartIndex = originalArgs.IndexOf('{', tagStartIndex + 1);
+    }
+
+    if (nextIndexToAdd < originalArgs.Length)
+      args.Append(originalArgs.AsSpan(nextIndexToAdd));
+
+    if (tagReplacedCount == 0)
+      args.Append($" \"{uri.OriginalString}\"");
+
+    return args.ToString();
   }
 }
